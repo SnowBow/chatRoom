20c4211d2c7276a85cd50eb2c849fe0572311b6a
吴继宝 <wujibao@wujibaodeMacBook-Pro.local>
2016-10-10 15:58:36 -0700

    MOD: move db to remote

--- public/js/dbconnection.js
+++ public/js/dbconnection.js
@@ -1,17 +1,20 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'chatApp'
-});
+// var connection = mysql.createConnection({
+//     host     : '127.0.0.1',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'chatApp',
+//     port: 3306
+// });
+var connection = mysql.createConnection(process.env.DATABASE_URL || 'mysql://b0412e5d99ce59:0c71bf65@us-cdbr-iron-east-04.cleardb.net/heroku_b0301d76f332155?reconnect=true');
+
 
 connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
-    console.log('connected as id ' + connection.threadId);
+    console.log('db connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
